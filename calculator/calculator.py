7a22916f4f983374a9b20eff117f873608f12d77
DEVA266 <devats266@gmail.com>
2025-04-06 16:25:08 +0530

    added

---
+++ calculator/calculator.py
@@ -0,0 +1,2 @@
+import std_cal.std_cal as std
+print(std.add(10,20))
---
+++ calculator/std_cal/std_cal.py
@@ -0,0 +1,35 @@
+import math
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a//b
+def modd(a,b):
+    return a%b
+def float_div(a,b):
+    return a/b
+def inverse(a):
+    return 1/a
+def negotate(a):
+    return (-1*a)
+def percentage(a,b):
+    # a refers to the value of the number 
+    # b refers to the percentage 
+    # b should be less than or equal to 100
+    if b<=100:
+        return (a*(b/100))
+def square(a):
+    return a**2
+def sqrt(a):
+    return math.sqrt(a)
+def pow10(a):
+    return a**10
+def pow(a,b):
+    # a is a base value
+    # b should be the power value
+    return a**b
+
+
